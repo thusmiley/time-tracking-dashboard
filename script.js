71cae26e8fa080d0533e5bcea12873abad0e9c92
Brenden Gerber <>
2023-02-10 09:13:18 -0600

    Fix default display and condense functions

--- script.js
+++ script.js
@@ -3,9 +3,8 @@ let Data = [];
 async function loadDefault() {
   const response = await fetch("./data.json");
   const parsedResponse = await response.json();
-  let updateData = (data) => Data.push(...data);
-  updateData(parsedResponse);
-  getWeekly();
+  Data.push(...parsedResponse);
+  getPeriod('weekly');
 }
 
 let card = document.querySelectorAll(".card");
@@ -13,71 +12,33 @@ let dailyBtn = document.getElementById("daily");
 let weeklyBtn = document.getElementById("weekly");
 let monthlyBtn = document.getElementById("monthly");
 
-function getDaily() {
-  dailyBtn.classList.add("active");
-  weeklyBtn.classList.remove("active");
-  monthlyBtn.classList.remove("active");
-
+function getPeriod(period) {
+  if(period === 'daily'){
+    dailyBtn.classList.add("active");
+    weeklyBtn.classList.remove("active");
+    monthlyBtn.classList.remove("active");
+  }else if(period === 'weekly'){
+    dailyBtn.classList.remove("active");
+    weeklyBtn.classList.add("active");
+    monthlyBtn.classList.remove("active");
+  }else if(period === 'monthly'){
+    dailyBtn.classList.remove("active");
+    weeklyBtn.classList.remove("active");
+    monthlyBtn.classList.add("active");
+  }
   for (let i = 0; i < Data.length; i++) {
     // This is for the edge case of "Self Care" to remove the space
     let splitTitle = Data[i].title.split("");
     splitTitle = splitTitle.filter((e) => String(e).trim());
     let joinTitle = splitTitle.join("");
-
+    
     let current = document.querySelector(`.${joinTitle.toLowerCase()}-current`);
     let previous = document.querySelector(
       `.${joinTitle.toLowerCase()}-previous`
-    );
-    current.innerHTML = `${Data[i].timeframes.daily.current + "hrs"}`;
-    previous.innerHTML = `${
-      "Yesterday - " + Data[i].timeframes.daily.previous + "hrs"
-    }`;
-
-    // card[i].innerHTML = ` <h3>${Data[i].timeframes.daily.current + "hrs"} </h3>
-    // <p>${"Yesterday - " + Data[i].timeframes.daily.previous + "hrs"} </p> `;
-  }
-}
-
-function getWeekly() {
-  dailyBtn.classList.remove("active");
-  weeklyBtn.classList.add("active");
-  monthlyBtn.classList.remove("active");
-
-  for (let i = 0; i < Data.length; i++) {
-    let splitTitle = Data[i].title.split("");
-    splitTitle = splitTitle.filter((e) => String(e).trim());
-    let joinTitle = splitTitle.join("");
-
-    let current = document.querySelector(`.${joinTitle.toLowerCase()}-current`);
-    let previous = document.querySelector(
-      `.${joinTitle.toLowerCase()}-previous`
-    );
-    current.innerHTML = `${Data[i].timeframes.weekly.current + "hrs"}`;
-    previous.innerHTML = `${
-      "Last Week - " + Data[i].timeframes.weekly.previous + "hrs"
-    }`;
-  }
-}
-
-function getMonthly() {
-  dailyBtn.classList.remove("active");
-  weeklyBtn.classList.remove("active");
-  monthlyBtn.classList.add("active");
-
-  for (let i = 0; i < Data.length; i++) {
-    let splitTitle = Data[i].title.split("");
-    splitTitle = splitTitle.filter((e) => String(e).trim());
-    let joinTitle = splitTitle.join("");
-
-    let current = document.querySelector(`.${joinTitle.toLowerCase()}-current`);
-    let previous = document.querySelector(
-      `.${joinTitle.toLowerCase()}-previous`
-    );
-    current.innerHTML = `${Data[i].timeframes.monthly.current + "hrs"}`;
-    previous.innerHTML = `${
-      "Last Month - " + Data[i].timeframes.monthly.previous + "hrs"
-    }`;
-  }
-}
+      );
+      current.innerHTML = `${Data[i].timeframes[period].current + "hrs"}`;
+      previous.innerHTML = `${"Yesterday - " + Data[i].timeframes[period].previous + "hrs"}`;
+    }
+};
 
 loadDefault();
